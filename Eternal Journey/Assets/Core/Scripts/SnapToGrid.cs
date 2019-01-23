b0ad2c003f67c952d1edcb1c1d5508f5d08f3d89
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-01-23 10:29:50 +0100

    Update SnapToGrid.cs
    disabled SnapToGrid for wip-state

--- Eternal Journey/Assets/Core/Scripts/SnapToGrid.cs
+++ Eternal Journey/Assets/Core/Scripts/SnapToGrid.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 public class SnapToGrid : MonoBehaviour 
 {
-  void OnSceneGUI() {
+  /*void OnSceneGUI() {
         Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
         this.x = Mathf.RoundToInt(ray.origin.x);
         this.y = Mathf.RoundToInt(ray.origin.y);
-  }
+  }*/
 }
