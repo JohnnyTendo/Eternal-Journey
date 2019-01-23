3a3af91afc84d37fa75b1c20a4d91aeae3d4d52d
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-01-23 10:18:05 +0100

    Create SnapToGrid.cs

---
+++ Eternal Journey/Assets/Core/Scripts/SnapToGrid.cs
@@ -0,0 +1,11 @@
+using Systems.generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour 
+{
+  void OnSceneGUI() {
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        this.x = Mathf.RoundToInt(ray.origin.x);
+        this.y = Mathf.RoundToInt(ray.origin.y);
+  }
+}
