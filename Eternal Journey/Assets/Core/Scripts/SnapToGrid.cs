f7ea311ea399528778a13be2b75ef3d8fab4b150
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-01-23 10:37:44 +0100

    Update SnapToGrid.cs

--- Eternal Journey/Assets/Core/Scripts/SnapToGrid.cs
+++ Eternal Journey/Assets/Core/Scripts/SnapToGrid.cs
@@ -5,7 +5,7 @@ public class SnapToGrid : MonoBehaviour
 {
   /*void OnSceneGUI() {
         Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-        this.x = Mathf.RoundToInt(ray.origin.x);
-        this.y = Mathf.RoundToInt(ray.origin.y);
+        this.x = Mathf.RoundToInt(ray.origin.x) * 10;
+        this.y = Mathf.RoundToInt(ray.origin.y) * 10;
   }*/
 }
