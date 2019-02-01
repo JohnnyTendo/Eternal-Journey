f0f027b1594ec02727a4612e6e9c4fbdfcd4cbfa
JohnnyTendo <JohnnyTendo@github.com>
2019-02-02 00:57:29 +0100

    finished build 0.0.2
    -adjustments
    -added bridge prefab
    -added chest prefab

---
+++ Eternal Journey/Assets/Core/Scripts/Interactables/Storage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : Interactable
+{
+
+    public bool isOpen = false;
+    public int slotCount = 20;
+
+    public override void Interact()
+    {
+        base.Interact();
+        isOpen = (isOpen) ? CloseStorage() : OpenStorage();
+    }
+
+    public bool OpenStorage()
+    {
+        //Some sort of inventory opens
+        //items should be moveable between inventories
+        return true;
+    }
+
+    public bool CloseStorage()
+    {
+        //close the inventory
+        return false;
+    }
+}
