e4439f5e1c6599fa8ea558c6d990c5e4a8c85da4
JohnnyTendo <40372786+JohnnyTendo@users.noreply.github.com>
2019-02-05 10:20:10 +0100

    exp storage

--- Eternal Journey/Assets/Core/Scripts/Interactables/Storage.cs
+++ Eternal Journey/Assets/Core/Scripts/Interactables/Storage.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Storage : Interactable
 {
-
+    public Transform itemsParent;
+    public GameObject inventoryUI2;
+    Inventory2 inventory2;
+    InventorySlot[] slots;
+    
     public bool isOpen = false;
     public int slotCount = 20;
-
+    
+    void Start()
+    {
+        inventory2 = Inventory2.instance;
+        inventory2.onItemChangedCallback += UpdateUI;
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+    }
+    
     public override void Interact()
     {
         base.Interact();
@@ -17,6 +28,7 @@ public class Storage : Interactable
     public bool OpenStorage()
     {
         //Some sort of inventory opens
+        inventoryUI2.SetActive(!inventoryUI2.activeSelf);
         //items should be moveable between inventories
         return true;
     }
@@ -25,5 +37,21 @@ public class Storage : Interactable
     {
         //close the inventory
         return false;
+    }
+    
+    void UpdateUI()
+    {
+        Debug.Log("Updating UI2");
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory2.items.Count)
+            {
+                slots[i].AddItem(inventory2.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
     }
 }
